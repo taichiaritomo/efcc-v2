244c50f193f2312bbeb590bc934f108d36b956e2
Taichi Aritomo <taichiaritomo@gmail.com>
2018-09-03 15:42:54 -0400

    google maps

--- js/maps.js
+++ js/maps.js
@@ -1,18 +1,23 @@
-
-function initialize() {
+  var map;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map-canvas'), {
+      center: {lat: 40.820017, lng: -73.977012},
+      zoom: 13
+    });
+    
+      map.setOptions({styles: styles});
+  
+  
   var origin = new google.maps.LatLng(40.819935,-73.977059);
-  var centerLat, centerLng;
-  if (document.documentElement.clientWidth >= 768) {
-    centerLat = 40.820017;
-    centerLng = -73.977012;
-  } else {
-    centerLat = 40.821;
-    centerLng = -73.97721360000003;
+  var marker = new google.maps.Marker({
+    position: origin,
+    map: map,
+    title: 'Edgewater Family Care'
+  });
+
+  marker.addListener('click', function() { window.open("https://www.google.com/maps/place/Edgewater+Family+Care/@40.8200375,-73.9771488,17.23z/data=!4m2!3m1!1s0x89c2f64c1c0ffb4d:0x2033da58fb023bff");
+  });
   }
-  var mapOptions = {
-    center: { lat: centerLat, lng: centerLng},
-    zoom: 13
-  };
 
   var styles = [
     {
@@ -28,19 +33,3 @@ function initialize() {
     },{
     }
   ];
-
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-
-  map.setOptions({styles: styles});
-
-  var marker = new google.maps.Marker({
-    position: origin,
-    map: map,
-    title: 'Edgewater Family Care'
-  });
-
-  marker.addListener('click', function() { window.open("https://www.google.com/maps/place/Edgewater+Family+Care/@40.8200375,-73.9771488,17.23z/data=!4m2!3m1!1s0x89c2f64c1c0ffb4d:0x2033da58fb023bff");
-  });
-}
-google.maps.event.addDomListener(window, 'load', initialize);
